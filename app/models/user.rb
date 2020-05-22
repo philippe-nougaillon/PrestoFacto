0393b43657013c7e3fad5cbeff840b1d923b7efa
philippe-nougailon <philippe.nougaillon@gmail.com>
2020-05-22 15:40:13 +0200

    User admin fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
-
   # Include default devise modules. Others available are:
   #  :timeoutable, :trackable and :omniauthable          
   #  :confirmable 
@@ -17,20 +16,19 @@ class User < ApplicationRecord
           :trackable, 
           :lockable
 
-
   belongs_to :organisation
 
   validates :email, presence: true
 
   self.per_page = 10
 
-private
-  def set_default_role
-    self.role ||= :user
-  end
-
   def admin? 
     self.role == 'admin'
   end
 
+private
+  def set_default_role
+    self.role ||= :user
+  end
+  
 end
