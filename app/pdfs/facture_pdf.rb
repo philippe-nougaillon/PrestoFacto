f847e0718e3aacad4ff41e38f53654e369e26eee
philippe-nougailon <philippe.nougaillon@gmail.com>
2020-05-27 15:19:16 +0200

    Amélioration facture PDF

--- app/pdfs/facture_pdf.rb
+++ app/pdfs/facture_pdf.rb
@@ -7,7 +7,7 @@ class FacturePdf
     end
 
     def initialize
-        @margin_down = 15
+        @margin_down = 11
         @image_path =  "#{Rails.root}/app/assets/images/"
     end
   
@@ -26,49 +26,68 @@ class FacturePdf
             end
         end
         bounding_box([300, y_position], :width => 300) do
-            text "Facture n° #{ facture.réf }" 
-            text "Statut : #{ facture.workflow_state.humanize }"
-            text "Date : #{ I18n.l facture.date.to_date }"
+            
+            formatted_text([
+                { text: 'Facture n° ', color: 'C0C0C0' },
+                { text: facture.réf  , styles: [:bold] }
+            ])
+            move_down @margin_down 
+
+            formatted_text [
+                { text: 'Statut : ', color: 'C0C0C0' },
+                { text: facture.workflow_state }
+            ]
+
+            formatted_text [
+                { text: 'Date : ', color: 'C0C0C0' },
+                { text: I18n.l(facture.date.to_date) }
+            ]
         end
         move_down @margin_down 
 
         y_position = cursor
         bounding_box([0, y_position], :width => 300) do
-            text "De :"
-            text organisation.nom
+            formatted_text [{ text: 'De :', color: 'C0C0C0' }]
+            formatted_text [{ text: organisation.nom, styles: [:bold] }] 
             text compte.structure.nom
             text organisation.adresse
             text "#{ organisation.cp } #{ organisation.ville }"  
             text organisation.téléphone
-            text organisation.email
+            # text organisation.email
         end
         bounding_box([300, y_position], :width => 300) do
-            text "Pour :"
-            text "#{ compte.civilité } #{ compte.nom }"
+            formatted_text [{ text: 'Pour :', color: 'C0C0C0' }]
+            formatted_text [{ text: "#{ compte.civilité } #{ compte.nom }", styles: [:bold] }] 
             text compte.adresse1
             text compte.adresse2
             text "#{ compte.cp } #{ compte.ville }"  
         end
-        move_down @margin_down * 3
+        move_down @margin_down * 5
 
-        data = [ ['Bénéficiaire', 'Prestation', 'Qté', 'Prix', 'Total'] ]
+        data = [ ['Bénéficiaire', 'Prestation', 'Qté', 'Prix.U', 'Total'] ]
   
         facture.facture_lignes.each do | ligne |
             data += [ [
-                    ligne.intitulé,
-                    ligne.prestation_type.nom,
-                    ligne.qté,
-                    "%5.2f" % ligne.prix,
-                    "%5.2f" % ligne.total
-            ] ]
+                        "<b>#{ligne.intitulé}</b>",
+                        ligne.prestation_type.nom,
+                        "%5.2f" % ligne.qté,
+                        "%5.2f €" % ligne.prix,
+                        "%5.2f €" % ligne.total
+                    ] ]
         end
 
-        data += [ [ nil, nil, nil, nil, facture.montant ] ]
+        data += [ [ nil, nil, nil, "Total : ", "%5.2f €" % facture.montant ] ]
 
-        table(data, 
+        table(
+            data, 
             header: true, 
             column_widths: [200, 100, 50, 50, 50], 
-            cell_style: { inline_format: true,  border_width: 0 }
+            cell_style: { 
+                            inline_format: true,  border_width: 1,
+                            borders: [:bottom],
+                            border_color: 'DEDEDE'
+                        }
+            
         )
 
     end
