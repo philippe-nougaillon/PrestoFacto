4d33dccb1d9e26707d23b419be3fdb1a08b60c13
philippe-nougailon <philippe.nougaillon@gmail.com>
2020-06-29 13:13:22 +0200

    smtp_settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -116,11 +116,11 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'prestofacto.heroku.com',
+    :domain         => 'prestofacto-demo.heroku.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { :host => 'prestofacto.herokuapp.com', :protocol => 'https' }
+  config.action_mailer.default_url_options = { :host => 'prestofacto-demo.herokuapp.com', :protocol => 'https' }
 
 end
