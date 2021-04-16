862177f13cacab785c97fa15039e7af0b209c9dd
Philippe Nougaillon <philippe.nougaillon@gmail.com>
2021-04-16 16:29:21 +0200

    v5.4.b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
 private
     def set_layout_variables
       @site_name = "PrestoFacto"
-      version = "v5.4.a"
+      version = "v5.4.b"
       @site_name_and_version = @site_name + ' ' + version
 
       @ctrl = params[:controller]
