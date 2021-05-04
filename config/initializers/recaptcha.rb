a964076942288715a71c61ddcb8c8878c431f1d0
Philippe Nougaillon <philippe.nougaillon@gmail.com>
2021-05-04 14:08:51 +0200

    Chaptcha env keys

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-    config.site_key  = '6Lc7ycUaAAAAAIU-92DJPLMhXZLWvJTp5DZZkeV4'
-    config.secret_key = '6Lc7ycUaAAAAAPBSO-D8s4cG4KrMtezpie7zMdQ9'
+    config.site_key  = ENV['RECAPTCHA_SITE_KEY']
+    config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
 end
