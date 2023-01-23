67b129538f7dd322cfaca7fd48d6639e5d183738
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2023-01-23 10:40:53 +0100

    PrestationType: destroy fixed

--- app/models/prestation_type.rb
+++ app/models/prestation_type.rb
@@ -5,6 +5,7 @@ class PrestationType < ApplicationRecord
 
   has_many :tarifs
   has_many :prestations
+  has_many :reservations
 
   validates :organisation_id, :nom, presence: true
   validates_uniqueness_of :nom, scope: [:organisation_id]
