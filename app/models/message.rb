fbff8e3125514d9a9dfdc398b5e79419d91a2acf
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2023-07-31 17:04:13 +0200

    Message : workflow added / enhanced

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,42 @@
 class Message < ApplicationRecord
+  audited
+
+  include Workflow
+  include WorkflowActiverecord
+
+  belongs_to :organisation, optional: true
+
   validates :email, :objet, :contenu, presence: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+
+  scope :ordered, -> { order(:updated_at)}
+  
+
+  # WORKFLOW
+  NOUVEAU   = 'nouveau'
+  TRAITE    = 'traité'
+  ARCHIVE   = 'archivé'
+
+  workflow do
+    state NOUVEAU, meta: { style: 'warning' } do
+      event :traiter, transitions_to: TRAITE
+    end
+
+    state TRAITE, meta: { style: 'success' } do
+      event :archiver, transitions_to: ARCHIVE
+    end
+
+    state ARCHIVE, meta: { style: 'secondary' }
+
+  end
+
+  # pour que le changement se voit dans l'audit trail
+  def persist_workflow_state(new_value)
+    self[:workflow_state] = new_value
+    save!
+  end
+
+  def style
+    self.current_state.meta[:style]
+  end
 end
