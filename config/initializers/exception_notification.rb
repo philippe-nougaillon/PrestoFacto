e50cd18c335ae4300299e8a1a8fa8b43ecc8da62
P-edacquet <p-edacquet@hotmail.fr>
2026-09-22 14:21:14 +0200

    Exception notification : \"BadRequest\", \"ParameterMissing\" and \"ParseError\" ignored (4xx client errors, bots)

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -5,7 +5,13 @@ require 'exception_notification/rails'
 ExceptionNotification.configure do |config|
   # Ignore additional exception types.
   # ActiveRecord::RecordNotFound, Mongoid::Errors::DocumentNotFound, AbstractController::ActionNotFound and ActionController::RoutingError are already added.
-  config.ignored_exceptions += %w[ActionController::InvalidAuthenticityToken]
+  # Erreurs de requête (4xx), en pratique des bots : pas de mail
+  config.ignored_exceptions += %w[
+    ActionController::InvalidAuthenticityToken
+    ActionController::BadRequest
+    ActionController::ParameterMissing
+    ActionDispatch::Http::Parameters::ParseError
+  ]
 
   # Adds a condition to decide when an exception must be ignored or not.
   # The ignore_if method can be invoked multiple times to add extra conditions.
